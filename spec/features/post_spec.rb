8597153e993e158dab320be6e7140e1c650a90d4
Nate Dalo <nate.dalo@yahoo.com>
2016-11-04 19:19:23 -0500

    Added new post button to navbar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -52,6 +52,15 @@ describe 'navigate' do
     end
   end
 
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
+      click_link("new_post_from_nav")
+      
+      expect(page.status_code).to eq(200)
+    end
+  end
+
   describe 'edit' do
     before do
       @post = create(:post, rationale: "Post1")
